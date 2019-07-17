873390e5e50c2150408a1a79eb818f0e584034d6
Marcos Dias <marcosdias.pvai@gmail.com>
2019-07-17 15:48:36 -0300

    aula javascript

---
+++ js/main.js
@@ -0,0 +1,60 @@
+//var d = new Date();
+
+//var count;
+//for(count=0, count <= 5, count++){
+//	alert(count);
+//};
+
+//for ( count = 0; count <= 5; count++ ) {
+//	alert(count);
+//};
+
+
+//var count = 0
+//while (count < 5){
+//	console.log(count);
+//	count = count + 1;
+//}
+
+
+
+//var idade = prompt("Qual sua idade?");
+
+//if(idade>18){
+//	alert("maior de 18 anos");
+//}else{
+//	alert("menor de 18 anos");
+//};
+
+
+
+//var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]; 
+//console.log(frutas.nome);
+//alert(frutas.cor);
+
+
+
+//var fruta = {nome: "maça", cor:"vermelho"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.pop("uva");
+//lista.pop();
+//console.log(lista.join(" - "));
+
+//var nome = "Marcos Dias";
+//var idade = 42; 
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos ");
+
+//alert(idade + idade2);
+
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log alert(frase.replace("Japão", "Brasil"));
